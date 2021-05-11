a0ac4a2001575520c59739bf87264be4bd83d81d
aldoboetd <aldoboetd908@gmail.com>
2021-05-12 03:38:49 +0700

    Fetching Product MongoDB

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const errorController = require("./controllers/error");
-const db = require("./util/database");
+const mongoConnect = require("./util/database").mongoConnect;
 
 const app = express();
 
@@ -14,20 +14,79 @@ app.set("views", "views");
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
-db.execute('SELECT * FROM products')
-  .then((result) => {
-    console.log(result);
-  })
-  .catch((err) => {
-    console.log(err);
-  });
-
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
+
+app.use((req, res, next) => {
+  // Using Sequelize
+  // User.findByPk(1)
+  //   .then((user) => {
+  //     req.user = user;
+  //     next();
+  //   })
+  //   .catch((err) => console.log(err));
+  next();
+});
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-app.listen(3000);
+// Mongodb
+mongoConnect(() => {
+  app.listen(3000);
+});
+
+// Using database SQL and package sequelize
+// const db = require("./util/database");
+// const sequelize = require("./util/database");
+// const Product = require("./models/product");
+// const User = require("./models/user");
+// const Cart = require("./models/cart");
+// const CartItem = require("./models/cart-item");
+// const Order = require("./models/order");
+// const OrderItem = require("./models/order-item");
+// Association
+// Product.belongsTo(User, { constraints: true, onDelete: "CASCADE" });
+// User.hasMany(Product);
+// User.hasOne(Cart);
+// Cart.belongsTo(User);
+// Cart.belongsToMany(Product, { through: CartItem });
+// Product.belongsToMany(Cart, { through: CartItem });
+// Order.belongsTo(User);
+// User.hasMany(Order);
+// Order.belongsToMany(Product, { through: OrderItem });
+
+// sequelize
+//   // .sync({ force: true })
+//   .sync()
+//   .then((result) => {
+//     return User.findByPk(1);
+//     // console.log(result);
+//   })
+//   .then((user) => {
+//     if (!user) {
+//       return User.create({ name: "Aldo", email: "test@test.com" });
+//     }
+//     return user;
+//   })
+//   .then((user) => {
+//     // console.log(user);
+//     return user.createCart();
+//   })
+//   .then((cart) => {
+//     app.listen(3000);
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
+
+// testing connect to database
+// db.execute('SELECT * FROM products')
+//   .then((result) => {
+//     console.log(result[0], result[1]);
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
--- util/database.js
+++ util/database.js
@@ -1,9 +1,50 @@
-const mqsql = require("mysql2");
+const mongodb = require("mongodb");
+const MongoClient = mongodb.MongoClient;
 
-const pool = mqsql.createPool({
-  host: 'localhost',
-  user: 'root',
-  database: 'node-complete',
-});
+let _db;
 
-module.exports = pool.promise();
+const mongoConnect = (callback) => {
+  MongoClient.connect(
+    "mongodb+srv://aldo:boetdboetd908@cluster0.wl7hq.mongodb.net/shop?retryWrites=true&w=majority"
+  )
+    .then((client) => {
+      console.log("Connected");
+      _db = client.db();
+      callback();
+    })
+    .catch((err) => {
+      console.log(err);
+      throw err;
+    });
+};
+
+const getDb = () => {
+  if (_db) {
+    return _db;
+  }
+  throw "No database found!";
+};
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
+
+// Connect database mysql using sequelize
+// const Sequelize = require("sequelize");
+
+// const sequelize = new Sequelize("node-complete", "root", null, {
+//   host: "localhost",
+//   dialect: "mysql",
+// });
+
+// module.exports = sequelize;
+
+// connected to database not use sequelize
+// const mqsql = require("mysql2");
+
+// const pool = mqsql.createPool({
+//   host: "localhost",
+//   user: "root",
+//   database: "node-complete",
+// });
+
+// module.exports = pool.promise();
